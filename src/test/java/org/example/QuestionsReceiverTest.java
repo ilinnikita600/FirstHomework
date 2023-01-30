2378173c55edfa7606549875efb082c4771340c8
Nikita Ilyin <ilinnikita600@gmail.com>
2023-01-30 20:38:51 +0300

    Delete tests

--- src/test/java/org/example/QuestionsReceiverTest.java
+++
@@ -1,9 +0,0 @@
-package org.example;
-
-import org.springframework.context.support.ClassPathXmlApplicationContext;
-import org.junit.jupiter.api.Assertions;
-
-import java.io.IOException;
-
-public class QuestionsReceiverTest {
-}
